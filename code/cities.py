b4bb7692e0520ec5629441cb6d232479ddcbb44b
pascuu7 <jaimepascuu@gmail.com>
2022-03-08 19:07:29 +0100

    listo para ejecutar checkins

--- code/cities.py
+++ code/cities.py
@@ -13,14 +13,13 @@ from matplotlib.pyplot import cla
 #   coordenadas = split_poi[1] y split_poi[2]
 #   codigo pais = split_poi[4]
 
-city_pois = open('prueba.txt', 'w')
+city_pois = open('POI_city.txt', 'w')
 
 cities = {}
 with open("dataset/cities.txt") as fcities:
     for line_city in fcities:
         split_city = line_city.split("\t")
-        # creamos el fichero
-        # open("city_files/" + str(split_city[3]) + "_" + str(split_city[0].replace(" ", "")) + ".txt", "w")
+        
         # añadimos los datos de la ciudad en la lista
         if str(split_city[3]) in cities.keys():
             cities[str(split_city[3])].append((split_city[0], split_city[1], split_city[2]))
@@ -33,8 +32,6 @@ with open("dataset/cities.txt") as fcities:
 
 
 i = 0
-
-# x = {str(ciudad): 3, str(segundo): "SISISISI"}
 
 with open("dataset/POIs.txt") as fpois:
     for line_poi in fpois:
@@ -49,30 +46,11 @@ with open("dataset/POIs.txt") as fpois:
                         min_dist = dist
                         ciudad = city[0]
                 i += 1
-                
-        # print(str(i) + '\t' + str(ciudad) + ': ' + str(min_dist) + 'km')
+        
         city_pois.write(str(i) + '\t' + str(split_poi[0]) + '\t' + str(split_poi[4].strip()) + "_" + str(ciudad.replace(" ", "")) + '\n')
         
         print(str(i) + '\t' + str(split_poi[0]) + '\t' + str(split_poi[4].strip()) + "_" + str(ciudad.replace(" ", "")))
         
-        # print(str(i) + " city_files/" + str(split_poi[4].strip()) + "_" + str(ciudad.replace(" ", "")) + ".txt")
-        # with open("city_files/"xt(split_poi[0], 16)) + "\t" + str(split_poi[1]) + "\t" + str(split_poi[2]) + "\t" + str(split_poi[3]) + "\t" + ciudad + "\n")
-                
-# int(s,16)
-        # for city in cities:
-        #     if city['pais'].strip() == split_poi[4].strip():
-        #         # funcion distancia
-        #         dist = distance((city['coorx'], city['coory']), (split_poi[1], split_poi[2])).km
-        #         # calcular minima
-        #         if dist < min_dist:
-        #             min_dist = dist
-        #             ciudad = city['nombre'] # ciudad de menor distancia
-
-        # solo para el print
-
-        # print(str(i) + '\t' + str(ciudad) + ': ' + str(min_dist) + 'km')
-
-# print(x)
 
             
             
--- code/prueba.py
+++ code/prueba.py
@@ -5,44 +5,31 @@ poi_dic = {}
 
 i = 0
 
+with open('data.txt', 'w') as fprueba:
+    fprueba.write("11222\t4bdaf5a23904a593d805489e\tWed Mar 27 17:40:17 +0000 2013\t120\n17690\t4b82d30cf964a52065e730e3\tWed Mar 27 17:40:18 +0000 2013\t120\n218711\t4b1aad22f964a5201def23e3\tWed Mar 27 17:40:21 +0000 2013\t-30\n135077\t4bc8351d0050b713e9f6b93b\tWed Mar 27 17:429793\t120\n140320\t4d83593202eb5481f15b17f5\tWed Mar 27 17:40:22 +0000 2013\t-180\n130643\t4fe13ceae4b01ac720143695\tWed Mar 27 17:40:23 +0000 2013\t-180\n112187\t4b292e69f964a5207a9a24e3\tWed Mar 27 17:40:24 +0000 2013\t120")
 with open('POI_city.txt') as fpois:
     for line_pois in fpois:
-        # print(line_pois)
         split_pois = line_pois.split("\t")
 
         poi_dic[str(split_pois[1])] = split_pois[2].strip() + '\t' + split_pois[0]
 
-with open('dataset/checkins.txt') as fcheck:
+with open('data.txt') as fcheck:
     for line_check in fcheck:
         split_check = line_check.split("\t")
+        print(str(i) + '\t' + str(split_check[0]))
 
         try:
             split_time = split_check[2].split(" ")
             
-            # inicio = time.time()
-
             s_datetime = split_time[2] + '/' + split_time[1] + '/' + split_time[5] + ' ' + split_time[3]
-                        # "03/04/2012 18:00:06"
 
             tiempo = int(datetime.datetime.strptime(s_datetime, '%d/%b/%Y %H:%M:%S').strftime("%s")) + 7200 
-            # - 60*int(split_check[3]) offset
 
-            # print(str(i) + '\t' + str(id) + '\t' + str(split_check[1]) + '\t' + ciudad)
-
-            # print(poi_dic[split_check[1]])
             city_id = poi_dic[split_check[1]].split("\t")
             id = city_id[1]
             fichero = "city_checkins/" + city_id[0] + ".txt"
-            # print(str(i) + '\t' + str(split_check[0]) + '\t' + id + '\t' + str(tiempo))
-            # print(ciudad + '\t' + id)
-
-            # id_user id_poi_mio timestamp
-            # print("city_checkins/" + poi_dic[split_check[1]].strip() + ".txt")
 
             i += 1
-            print(str(i) + '\t' + str(split_check[0]) + '\t' + id + '\t' + str(tiempo))
-            
-            # print("city_checkins/" + ciudad + ".txt")
 
 
             if os.path.exists(fichero):
@@ -53,15 +40,6 @@ with open('dataset/checkins.txt') as fcheck:
             else:
                 with open(fichero, "w") as fcities:
                     fcities.write(str(split_check[0]) + '\t' + id + '\t' + str(tiempo) + '\n')
-            
-            # with open(fichero, "a") as fcities:
-            #     fcities.write(str(split_check[0]) + '\t' + id + '\t' + str(tiempo) + '\n')
-
-            # print(str(i) + '\t' +  str(split_check))      
-                    
-            # fin = time.time()
-            # print(fin-inicio)
-            # break
         except:
             pass
         
