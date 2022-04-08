678ba455bf228815577d0c602aa118e2dd2011d8
pascuu7 <jaimepascuu@gmail.com>
2022-04-08 14:42:38 +0200

    coincidencias comentado

--- code/coincidencias/coincidencias_pois.py
+++ code/coincidencias/coincidencias_pois.py
@@ -1,6 +1,8 @@
-import time
 import math
 import os
+
+# utilizamos la funcion haversine para calcular la distancia entre dos coordenadas
+# aunque en esta multiplicamos por 1000n ya que la vamos a utilizar en metros
 
 def haversine(lat1, lon1, lat2, lon2):
     rad=math.pi/180
@@ -11,17 +13,22 @@ def haversine(lat1, lon1, lat2, lon2):
     distancia=2*R*math.asin(math.sqrt(a))
     return distancia*1000
 
-contenido = os.listdir('Gowalla/city_checkins/')
+# guardamos en contenido la lista de ficheros con los checkins de Gowalla
+contenido = os.listdir('../Gowalla/city_checkins/')
 
+# guardamos en un diccionarios pois correspondientes a cada ciudad con sus respectivas coordenadas
 poi_ny = {} # id: lat\tlong
 poi_tokyo = {} # id: lat\tlong
-coincidencias = {} # id Gow: id Four
 
-i = 0
-
-with open('Foursquare/POI_city.txt') as poi_four:
+# recorremos el fichero de los pois de Foursquare guardandolos en el diccionario correspondiente 
+with open('../Foursquare/POI_city.txt') as poi_four:
     for line_poi in poi_four:
         split_poi = line_poi.split("\t")
+        # 0: id nuestro
+        # 1: id foursquare
+        # 2: latitud
+        # 3: longitud
+        # 4: ciudad 
 
         if split_poi[4].strip() == "US_NewYork":
             poi_ny[str(split_poi[0])] = (split_poi[2], split_poi[3])
@@ -30,16 +37,32 @@ with open('Foursquare/POI_city.txt') as poi_four:
             poi_tokyo[str(split_poi[0])] = (split_poi[2], split_poi[3])
 
 
-
-with open('Gowalla/POI_city.txt') as poi_gow:
-    
+# recorremos el fichero de pois de Gowalla para ver las correspondencias
+with open('../Gowalla/POI_city.txt') as poi_gow:
     for line_poi in poi_gow:
         split_poi = line_poi.split("\t")
+        # 0: id nuestro
+        # 1: latitud
+        # 2: longitud
+        # 3: ciudad
 
+        # distancia minima para que coincidan los pois
+        min_dist = 5
+
+        # comprobamos a que ciudad pertenece el poi para utilizar el diccionario correspondiente
         if split_poi[3].strip() == 'US_NewYork':
             for key in poi_ny:
+                # calculamos la distancia entre los pois
                 dist = haversine(float(poi_ny[key][0]), float(poi_ny[key][1]), float(split_poi[1]), float(split_poi[2]))
-                if dist < 5:
+                # si es menor de 5 metros y menor que la distacia anterior, guarda los ids en coincidencias. Habrá
+                # repetidos en coincidencias_pois pero como luego se irán añadiendo en un diccionario, solo se
+                # guardará la ultima coincidencia vista, es decir, la de menor distancia
+                if dist < 5 and dist < min_dist:
+                    # se actualiza la distancia minima
+                    min_dist = dist
+
+                    # coincidencias_pois.txt:
+                    # id_Gowalla    id_Foursquare
                     if os.path.exists("coincidencias_pois.txt"):
                         with open("coincidencias_pois.txt", "a") as fcoin:
                             fcoin.write(str(split_poi[0]) + '\t' + str(key) + '\n')
@@ -48,19 +71,18 @@ with open('Gowalla/POI_city.txt') as poi_gow:
                         with open("coincidencias_pois.txt", "w") as fcoin:
                             fcoin.write(str(split_poi[0]) + '\t' + str(key) + '\n')
           
+        # mismo procedimiento para Tokyo
         else:
            for key in poi_tokyo:
                 
                 dist = haversine(float(poi_tokyo[key][0]), float(poi_tokyo[key][1]), float(split_poi[1]), float(split_poi[2]))
-                if dist < 5:
+                if dist < 5 and dist < min_dist:
+                    min_dist = dist
                     if os.path.exists("coincidencias_pois.txt"):
                         with open("coincidencias_pois.txt", "a") as fcoin:
                             fcoin.write(str(split_poi[0]) + '\t' + str(key) + '\n')
                     else:
                         with open("coincidencias_pois.txt", "w") as fcoin:
                             fcoin.write(str(split_poi[0]) + '\t' + str(key) + '\n')
-        
-        i += 1
-        print(str(i) + str(split_poi))
 
         
--- code/coincidencias/coincidencias_users.py
+++ code/coincidencias/coincidencias_users.py
@@ -1,44 +1,47 @@
 import os
 
-i = 0
+# guardamos en contenido la lista de ficheros con los checkins de Foursquare
+contenido = os.listdir('../Foursquare/city_checkins/')
 
-contenido = os.listdir('Foursquare/city_checkins/')
-
+# listas con los id de los usuarios de Foursquare y Gowalla
 four_users = []
-users = {}
 gow_users = []
 
+# recorremos los fichero de checkins de Foursquare para hacer una lista de los usuarios
 for name in contenido:
-    check_four_file = 'Foursquare/city_checkins/' + name
-    file = 'city_checkins/' + name
+    check_four_file = '../Foursquare/city_checkins/' + name
     
     with open(check_four_file) as check_four:
         for line_four in check_four:
-            i += 1
-            print(i)
             split_check = line_four.split("\t")
+            # 0: id usuario
+            # 1: id poi
+            # 2: timestamp
+
+            # añadimos el usuario a la lista
             four_users.append(int(split_check[0]))
-            if os.path.exists(file):
-                with open(file, "a") as fcheck:
-                    fcheck.write(line_four)
 
-            else:
-                with open(file, "w") as fcheck:
-                    fcheck.write(line_four)
-
+# cogemos el valor más alto de usuario para continuar con el siguiente en Gowalla
 max_user = max(four_users)
 
-
+# recorremos los ficheros de checkins de Gowalla
 for name in contenido:
-    check_gow_file = 'Gowalla/city_checkins/' + name
-    i += 1
+    check_gow_file = '../Gowalla/city_checkins/' + name
     with open(check_gow_file) as check_gow:
         for line_gow in check_gow:
             split_check = line_gow.split("\t")
+            # 0: id usuario
+            # 1: id poi
+            # 2: timestamp
+
+            # si no está todavía en la lista de usuarios visitados, lo añadimos y lo visitamos
             if split_check[0] not in gow_users:
                 gow_users.append(split_check[0])
+                # incrementamos en 1 el id a asignar
                 max_user += 1
 
+                # coincidencias_users.txt:
+                # id_Gowalla    id_asignado
                 if os.path.exists('coincidencias_users.txt'):
                     with open('coincidencias_users.txt', "a") as users:
                         users.write(str(split_check[0]) + '\t' + str(max_user) + '\n')
@@ -46,9 +49,6 @@ for name in contenido:
                 else:
                     with open('coincidencias_users.txt', "w") as users:
                         users.write(str(split_check[0]) + '\t' + str(max_user) + '\n')
-
-
-    print(i)
 
 
         
