103d55084a0815eaf22392b4126774c2ef29c0ea
pascuu7 <jaimepascuu@gmail.com>
2022-04-28 18:49:38 +0200

    popularidad hecho y train_test comentado

--- code/recomendation/popularidad/popularidad.py
+++ code/recomendation/popularidad/popularidad.py
@@ -1,3 +1,6 @@
+""" Se encarga de hacer la recomendación al usuario de los 50 pois más populares
+según los pois que no ha visitado """
+
 from genericpath import exists
 import pandas as pd
 
@@ -5,15 +8,45 @@ import pandas as pd
 ny_rating = {} # id_poi: rating_total
 tk_rating = {}
 
+recomended_ny = {}
+recomended_tk = {}
+
 # pois que ha visitado el usuario
-visits = {} # id_user: [id_po1, id_poi2, ...]
+# visits = {} # id_user: [id_po1, id_poi2, ...]
 
-cities = []
-i = 0
+# pois que ha visitado el usuario
+pois = []
+count = 0
 
-# in_user = input("Usuario: ")
-# in_city = input("City: ")
+user_in = input("Usuario: ")
 
+# Recorremos los 2 ficheros añadiendo a pois los pois que ya ha visitado el usuario
+with open('../train_test/US_NewYork/US_NewYork_train.txt') as ny_train:
+    for line_ny in ny_train:
+        split_ny = line_ny.split("\t")
+        # 0: user
+        # 1: poi
+        # 2: timestamp
+        # 3: rating
+        
+        # si coincide el usuario, añadimos el poi
+        if user_in == split_ny[0]:
+            pois.append(split_ny[1])
+
+with open('../train_test/JP_Tokyo/JP_Tokyo_train.txt') as tk_train:
+    for line_tk in tk_train:
+        split_tk = line_tk.split("\t")
+        # 0: user
+        # 1: poi
+        # 2: timestamp
+        # 3: rating
+
+        # si coincide el usuario, añadimos el poi
+        if user_in == split_tk[0]:
+            pois.append(split_tk[1])
+
+# volvemos a recorrer los ficheros guardando en el diccionario la suma de los 
+# ratings de cada poi
 with open('../train_test/US_NewYork/US_NewYork_train.txt') as ny_train:
     for line_ny in ny_train:
         split_ny = line_ny.split("\t")
@@ -22,15 +55,12 @@ with open('../train_test/US_NewYork/US_NewYork_train.txt') as ny_train:
         # 2: timestamp
         # 3: rating
 
-        i += 1
-        print(i)
+        # visits[split_ny[0]].append(split_ny[1])
 
-        visits[split_ny[0]].append(split_ny[1])
-
-       
-
+        # si hemos visitado el poi, sumamos el rating que hay con el nuevo
         if split_ny[1] in ny_rating.keys():
             ny_rating[split_ny[1]] = int(ny_rating[split_ny[1]]) + int(split_ny[3].strip())
+        # si no lo hemos visitado lo guardamos
         else:
             ny_rating[split_ny[1]] = int(split_ny[3].strip())
 
@@ -42,16 +72,38 @@ with open('../train_test/JP_Tokyo/JP_Tokyo_train.txt') as tk_train:
         # 2: timestamp
         # 3: rating
 
-        visits[split_tk[0]].append(split_tk[1])
+        # visits[split_tk[0]].append(split_tk[1])
 
+        # si hemos visitado el poi, sumamos el rating que hay con el nuevo
         if split_tk[1] in tk_rating.keys():
             tk_rating[split_tk[1]] = int(tk_rating[split_tk[1]]) + int(split_tk[3].strip())
+        # si no lo hemos visitado lo guardamos
         else:
             tk_rating[split_tk[1]] = int(split_tk[3].strip())
 
+# ordenamos los diccionarios de mayor a menor según su rating
 sorted_ny = sorted(ny_rating.items(), key=lambda x: x[1], reverse=True)
+sorted_tk = sorted(tk_rating.items(), key=lambda x: x[1], reverse=True)
 
-print(sorted_ny)
+
+for poi in sorted_ny:
+    if poi not in pois:
+        count += 1
+        recomended_ny[poi[0]] = poi[1]
+
+    if count == 50:
+        count = 0
+        break
+   
+for poi in sorted_tk:
+    if poi not in pois:
+        count += 1
+        recomended_tk[poi[0]] = poi[1]
+
+    if count == 50:
+        count = 0
+        break
+   
 
 # if in_city == 'New York':
 #     for poi in ny_rating:
@@ -59,4 +111,3 @@ print(sorted_ny)
 #             cities.append()
 
 
-sorted_tk = sorted(tk_rating.items(), key=lambda x: x[1], reverse=True)
--- code/recomendation/train_test.py
+++ code/recomendation/train_test.py
@@ -1,20 +1,27 @@
+""" Se encarga de crear los ficheros correspondientes, dividiendo aleatoriamente los 
+los datos entre train y test """
+
 import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 
-contenido = os.listdir('../dataset/user_ratings/')
+# listado de ficheros de ratings de Foursquare
+contenido = os.listdir('../dataset/Foursquare/user_ratings/')
 
+# recorremos cada fichero
 for name in contenido:
-    fichero = '../dataset/user_ratings/' + name 
+    fichero = '../dataset/Foursquare/user_ratings/' + name 
+    # guardamos el nomnbre de la ciudad que estamos visitando
     ciudad = name.replace('.txt', '')
     
+    # para leer el fichero usamos pandas
     datos = pd.read_csv(fichero, sep='\t', header=None)
-
     df = pd.DataFrame(datos)
 
-
+    # separamos entre train y test siendo test un 20%
     train, test = train_test_split(df, test_size=0.2)
 
+    # nombre de los nuevos ficheros
     ftrain = 'train_test/' + ciudad + '/' + ciudad + '_train.txt'
     ftest = 'train_test/' + ciudad + '/' + ciudad + '_test.txt'
 
