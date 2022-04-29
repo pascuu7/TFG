ca7a5d7184c9b445a83fcbdc63dbb1c25a0fe0bb
pascuu7 <jaimepascuu@gmail.com>
2022-04-29 10:44:16 +0200

    Popularidad con dudas

--- code/recomendation/popularidad/popularidad.py
+++ code/recomendation/popularidad/popularidad.py
@@ -1,14 +1,12 @@
 """ Se encarga de hacer la recomendación al usuario de los 50 pois más populares
 según los pois que no ha visitado """
 
-from genericpath import exists
-import pandas as pd
-
 # suma de ratings de cada poi
 ny_rating = {} # id_poi: rating_total
 tk_rating = {}
 
-recomended_ny = {}
+# diccionario con los pois a recomendar
+recomended_ny = {} # id_poi: rating
 recomended_tk = {}
 
 # pois que ha visitado el usuario
@@ -16,11 +14,12 @@ recomended_tk = {}
 
 # pois que ha visitado el usuario
 pois = []
-count = 0
 
+# usuario a recomendar
 user_in = input("Usuario: ")
 
-# Recorremos los 2 ficheros añadiendo a pois los pois que ya ha visitado el usuario
+# Recorremos los 2 ficheros añadiendo a pois los pois que ya ha visitado el usuario    
+# y para guardar en el diccionario la suma de los ratings de cada poi
 with open('../train_test/US_NewYork/US_NewYork_train.txt') as ny_train:
     for line_ny in ny_train:
         split_ny = line_ny.split("\t")
@@ -28,34 +27,15 @@ with open('../train_test/US_NewYork/US_NewYork_train.txt') as ny_train:
         # 1: poi
         # 2: timestamp
         # 3: rating
-        
+
+
+#??????????????????????
+        # visits[split_ny[0]].append(split_ny[1])
+#?????????????????????? 
+
         # si coincide el usuario, añadimos el poi
         if user_in == split_ny[0]:
             pois.append(split_ny[1])
-
-with open('../train_test/JP_Tokyo/JP_Tokyo_train.txt') as tk_train:
-    for line_tk in tk_train:
-        split_tk = line_tk.split("\t")
-        # 0: user
-        # 1: poi
-        # 2: timestamp
-        # 3: rating
-
-        # si coincide el usuario, añadimos el poi
-        if user_in == split_tk[0]:
-            pois.append(split_tk[1])
-
-# volvemos a recorrer los ficheros guardando en el diccionario la suma de los 
-# ratings de cada poi
-with open('../train_test/US_NewYork/US_NewYork_train.txt') as ny_train:
-    for line_ny in ny_train:
-        split_ny = line_ny.split("\t")
-        # 0: user
-        # 1: poi
-        # 2: timestamp
-        # 3: rating
-
-        # visits[split_ny[0]].append(split_ny[1])
 
         # si hemos visitado el poi, sumamos el rating que hay con el nuevo
         if split_ny[1] in ny_rating.keys():
@@ -72,7 +52,12 @@ with open('../train_test/JP_Tokyo/JP_Tokyo_train.txt') as tk_train:
         # 2: timestamp
         # 3: rating
 
+#?????????????????????? 
         # visits[split_tk[0]].append(split_tk[1])
+#?????????????????????? 
+        # si coincide el usuario, añadimos el poi
+        if user_in == split_tk[0]:
+            pois.append(split_tk[1])
 
         # si hemos visitado el poi, sumamos el rating que hay con el nuevo
         if split_tk[1] in tk_rating.keys():
@@ -85,25 +70,25 @@ with open('../train_test/JP_Tokyo/JP_Tokyo_train.txt') as tk_train:
 sorted_ny = sorted(ny_rating.items(), key=lambda x: x[1], reverse=True)
 sorted_tk = sorted(tk_rating.items(), key=lambda x: x[1], reverse=True)
 
-
+# recorremos el diccionario y por cada poi comprobamos si lo ha visitado el usuario
 for poi in sorted_ny:
+    # si no lo ha visitado, lo guardamos en el diccionario de recomendados
     if poi not in pois:
-        count += 1
         recomended_ny[poi[0]] = poi[1]
 
-    if count == 50:
-        count = 0
+    # si ya se ha recomendado 50, paramos
+    if len(recomended_ny) == 50:
         break
    
+# recorremos el diccionario y por cada poi comprobamos si lo ha visitado el usuario
 for poi in sorted_tk:
+    # si no lo ha visitado, lo guardamos en el diccionario de recomendados
     if poi not in pois:
-        count += 1
         recomended_tk[poi[0]] = poi[1]
 
-    if count == 50:
-        count = 0
+    # si ya se ha recomendado 50, paramos
+    if len(recomended_tk) == 50:
         break
-   
 
 # if in_city == 'New York':
 #     for poi in ny_rating:
